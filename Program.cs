e340b446a92e42e662dabc4a166a056fc1b475d3
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2019-12-19 16:34:24 +0500

    New Changes

--- Program.cs
+++ Program.cs
@@ -1,32 +1,19 @@
 ﻿using OpenQA.Selenium;
-
 using OpenQA.Selenium.Chrome;
-
 using System;
 using System.Threading;
 
 namespace First_Test
-
 {
-
     class Entrypoint
     {
-
         static void Main()
         {
-
             IWebDriver driver = new ChromeDriver("./");
-
             //NuGet package which will copy the driver to the bin folder.
             //Then you can provide path to the current folder to driver ctor to run your tests:
-
-
             driver.Navigate().GoToUrl("http://116.203.214.214/");
-
-
-
             IWebElement Userid = driver.FindElement(By.Name("email"));
-
             Userid.Click();
             Userid.SendKeys("test@abc.com");
             Thread.Sleep(3000);
@@ -35,10 +22,8 @@ namespace First_Test
             password.Click();
             password.SendKeys("abc");
             Thread.Sleep(3000);
-
-
-
             IWebElement Button = driver.FindElement(By.XPath("/html/body/div/div/div/div/div/div/form/div[5]/button"));
+
             if (Userid.Displayed)
             {
 
@@ -50,12 +35,10 @@ namespace First_Test
             {
                 Redmessage("The element not found");
             }
-
             Button.Click();
             Thread.Sleep(3000);
             driver.Close();
             driver.Quit();
-
         }
 
         private static void Greenmessage(string v)
@@ -63,7 +46,6 @@ namespace First_Test
             Console.ForegroundColor = ConsoleColor.Green;
             System.Console.WriteLine(v);
             Console.ForegroundColor = ConsoleColor.White;
-
         }
 
         private static void Redmessage(string x)
@@ -73,6 +55,6 @@ namespace First_Test
             Console.ForegroundColor = ConsoleColor.White;
         }
 
-       
+
     }
 }
